6b1150f7bb7b13a5639fecebb192145336c718cb
Ogit <oritgurel@gmail.com>
2018-02-13 15:09:59 +0200

    initial commit

--- app/src/main/java/com/example/user/youtubeapi/YouTubeApi/Service.java
+++ app/src/main/java/com/example/user/youtubeapi/YouTubeApi/Service.java
@@ -1,16 +1,21 @@
 package com.example.user.youtubeapi.YouTubeApi;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by User on 13/02/2018.
  */
 
 public interface Service {
+   static final String query = "v=";
+   String videoLink="";
+
 
     @GET("http://www.leado.co.il/clients/shahak/json.json")
-    Call<List<ListItem>> getListItems();
+    Call<YouTubeApiResponse> getResponse();
+
+    @GET(videoLink)
+    Call<ListItem> getLinkId(@Query(query) String query);
 }
