6d8399ba1520447b80df50d863cd3674737bc5b1
Ogit <oritgurel@gmail.com>
2018-02-13 14:49:27 +0200

    initial commit

--- app/src/main/java/com/example/user/youtubeapi/PlaylistItemViewActivity.java
+++ app/src/main/java/com/example/user/youtubeapi/PlaylistItemViewActivity.java
@@ -4,15 +4,26 @@ package com.example.user.youtubeapi;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by User on 12/02/2018.
  */
 
 public class PlaylistItemViewActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_items_view);
+        recyclerView = (findViewById(R.id.recycler_view));
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
     }
 }
---
+++ app/src/main/java/com/example/user/youtubeapi/VideoItemsAdapter.java
@@ -0,0 +1,96 @@
+package nanofaroque.com.youtubeplayertesting;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.user.youtubeapi.R;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailView;
+/**
+ * Created by ofaroque on 8/13/15.
+ */
+public class VideoItemsAdapter extends RecyclerView.Adapter<VideoItemsAdapter.VideoInfoHolder> {
+
+    //these ids are the unique id for each video
+    String[] VideoID = {"P3mAtvs5Elc", "nCgQDjiotG0", "P3mAtvs5Elc"};
+    Context ctx;
+
+    public VideoItemsAdapter(Context context) {
+        this.ctx = context;
+    }
+
+    @Override
+    public VideoInfoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.video_item, parent, false);
+        return new VideoInfoHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final VideoInfoHolder holder, final int position) {
+
+
+        final YouTubeThumbnailLoader.OnThumbnailLoadedListener  onThumbnailLoadedListener = new YouTubeThumbnailLoader.OnThumbnailLoadedListener(){
+            @Override
+            public void onThumbnailError(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader.ErrorReason errorReason) {
+
+            }
+
+            @Override
+            public void onThumbnailLoaded(YouTubeThumbnailView youTubeThumbnailView, String s) {
+                youTubeThumbnailView.setVisibility(View.VISIBLE);
+                holder.relativeLayoutOverYouTubeThumbnailView.setVisibility(View.VISIBLE);
+            }
+        };
+
+        holder.youTubeThumbnailView.initialize(Resources.KEY, new YouTubeThumbnailView.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader youTubeThumbnailLoader) {
+
+                youTubeThumbnailLoader.setVideo(VideoID[position]);
+                youTubeThumbnailLoader.setOnThumbnailLoadedListener(onThumbnailLoadedListener);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView, YouTubeInitializationResult youTubeInitializationResult) {
+                //write something for failure
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return VideoID.length;
+    }
+
+    public class VideoInfoHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        protected RelativeLayout relativeLayoutOverYouTubeThumbnailView;
+        YouTubeThumbnailView youTubeThumbnailView;
+        protected ImageView playButton;
+
+        public VideoInfoHolder(View itemView) {
+            super(itemView);
+            playButton=(ImageView)itemView.findViewById(R.id.btnYoutube_player);
+            playButton.setOnClickListener(this);
+            relativeLayoutOverYouTubeThumbnailView = (RelativeLayout) itemView.findViewById(R.id.relativeLayout_over_youtube_thumbnail);
+            youTubeThumbnailView = (YouTubeThumbnailView) itemView.findViewById(R.id.youtube_thumbnail);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = YouTubeStandalonePlayer.createVideoIntent((Activity) ctx, Resources.KEY, VideoID[getLayoutPosition()]);
+            ctx.startActivity(intent);
+        }
+    }
+}
